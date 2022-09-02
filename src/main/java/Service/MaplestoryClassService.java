4fabfe14067bb1bd4feabc1261f6293ba60c8acf
Jamie <jnguyen127@horizon.csueastbay.edu>
2022-09-01 21:20:19 -0700

    MaplestoryClassRepository

--- src/main/java/Service/MaplestoryClassService.java
+++ src/main/java/Service/MaplestoryClassService.java
@@ -1,4 +1,15 @@
 package Service;
 
+import DOA.MaplestoryClassRepository;
+
 public class MaplestoryClassService {
+    MaplestoryClassRepository mcr;
+
+    public MaplestoryClassService() {
+        mcr = new MaplestoryClassRepository();
+    }
+
+    public String getClassNameFromID(int id) {
+        return mcr.getClassNameFromID(id);
+    }
 }
